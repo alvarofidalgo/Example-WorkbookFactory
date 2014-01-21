03382ed867ed8fd38488164774dbd8b9e48966a6
alvarofidalgo <dmj20@hotmail.com>
2014-01-21 20:09:35 +0100

    Ejecucion de todos los test

---
+++ src/test/excel/allTest/RunAllTest.java
@@ -0,0 +1,9 @@
+package test.excel.allTest;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+public class RunAllTest {
+
+}
